1ba55a27e39fd59716d7a41db786c58037caca46
Omer Sozcu <omerfsozcu@gmail.com>
2024-11-27 11:59:36 +0100

    Test-2 is added

--- GitHubExample11/Program.cs
+++ GitHubExample11/Program.cs
@@ -7,3 +7,6 @@ Console.WriteLine("This is master branch");
 
 // This lines are from testbranch-1
 Console.WriteLine("Testbranch-1 is added");
+
+// This lines are from testbranch-2
+Console.WriteLine("Testbranch-2 is added");
